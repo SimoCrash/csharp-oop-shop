aefb9f90f03b59e5c8a76b542b9ad90e44e48535
SimoCrash <simone.crescenzi@hotmail.com>
2023-03-20 16:13:49 +0100

    push1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿var mioProdotto = new Prodotto();
+﻿using csharp_oop_shop;
+
+Prodotto cellulare = new Prodotto();
+
+cellulare.smartPhone(23435364, "OnePlus", "anno 2021", 399.00, 22);
+Console.WriteLine(cellulare);
+
+
+
+//Nel progetto csharp-oop-shop, creare la classe Prodotto che gestisce i prodotti dello shop.
+//Un prodotto è caratterizzato da:
+//-codice(numero intero)
+//- nome
+//- descrizione
+//- prezzo
+//- iva
+//Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che:
+//alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+//Il codice prodotto sia accessibile solo in lettura
+//Gli altri attributi siano accessibili sia in lettura che in scrittura
+//Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
+//Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
+//Testate poi i vostri oggetti Prodotto, istanziandoli e provando ad interargirci per testare tutti i metodi che avete previsto.
